2cd0160b1f7e402f6e90f6bb6cb67a4b48b8a8fb
CH_Hwang <hch950627@naver.com>
2022-02-05 13:34:33 +0900

    [edit] github action node version 16

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -9,14 +9,6 @@ import Search from '../pages/Search';
 import { isToken } from '../util/getToken';
 
 function App() {
-  console.log(
-    '%c                ',
-    'font-size:100px; background:url(https://user-images.githubusercontent.com/86486778/148679216-0d895bca-7499-4c67-9a80-93e295d7650c.png) no-repeat center;'
-  );
-  console.log(
-    '%c  Link Gather  ',
-    'font-size: 100px; font-weight:bolder; background-color:black; color:rgb(255,255,255,1);'
-  );
   const [isLogin, setIsLogin] = useState(false);
   const setState = (state: boolean) => setIsLogin(state);
   const history = useHistory();
