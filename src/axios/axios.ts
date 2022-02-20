5a2ccd5583831a1acee31c9713f14eeae33eecd8
CH_Hwang <hch950627@naver.com>
2022-02-20 18:46:08 +0900

    [fix] api 주소 수정

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -3,7 +3,7 @@ import { SigninData, SignupData, submitPostData } from '../interfaces/data';
 import { getToken } from '../util/getToken';
 
 const api = axios.create({
-  baseURL: 'https://linkgather.shop',
+  baseURL: 'https://linkgather.shop/api',
   headers: {
     'content-type': 'application/json;charset=UTF-8',
     'accept': 'application/json',
