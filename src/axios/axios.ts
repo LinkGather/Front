682b03bed15561ab28371c3f1975e6369a8b0fd7
changhwan hwang <changhwanhwang@changhwanui-MacBookPro.local>
2022-02-21 09:37:04 +0900

    [edit]주소변경

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -3,7 +3,7 @@ import { SigninData, SignupData, submitPostData } from '../interfaces/data';
 import { getToken } from '../util/getToken';
 
 const api = axios.create({
-  baseURL: 'https://linkgather.shop/api',
+  baseURL: 'https://linkgather.shop',
   // baseURL: 'http://localhost:3001',
   headers: {
     'content-type': 'application/json;charset=UTF-8',
